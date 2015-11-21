2ed719c36ea3f5508b788439e745b0f879e951be
rafeh01 <rafehqazi1@gmail.com>
2015-11-21 05:15:42 -0600

    added game rules notes in simulation_racquetball.py.

---
+++ intro_To_CS/python_programming_by_john_zelle/9_chapter_simulation/simulation_racquetball.py
@@ -0,0 +1,15 @@
+"""
+A program to simulate racquetball
+Author: Rafeh Qazi
+Date: 11/21/2015
+"""
+
+# Racquetball Game Rules:
+# 1. First move is called the Serve.
+# 2. Alternating turns with legal moves is called a Rally.
+# 3. Rally ends when one player fails to hit a legal shot.
+# 4. Player who misses the shot is the loser of the rally.
+# 5. If server loses the point, serve switches.
+# 6. If server wins the point, a point is awarded.
+# 7. Players can only score during their own service.
+# 8. First player to reach 15 points first wins the game.
